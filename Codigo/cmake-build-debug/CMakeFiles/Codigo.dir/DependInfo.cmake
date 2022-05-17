aa4b21895c9b26ced29b1199bb8d1c895d9c5606
Deylan Alexander <deylan6@gmail.com>
2022-05-16 18:14:20 -0600

    hola

---
+++ Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/BMP/Bmp.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/BMP/Bmp.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Circulo.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Circulo.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Cuadrado.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Cuadrado.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Facade_funciones.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Facade_funciones.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Figuras.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Figuras.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Filtros.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Filtros.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Funcionalidades/Giros.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Funcionalidades/Giros.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Interfaz/Componentes.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Interfaz/Componentes.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Interfaz/Datos_juego.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Interfaz/Datos_juego.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/Interfaz/Ventana.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Interfaz/Ventana.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Lista_Doble.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Lista_Doble.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Lista_pixeles.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Lista_pixeles.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Matriz.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Matriz.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Nodo.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Nodo_matriz.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo_matriz.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/lista doblemente enlazada/Nodo_pixel.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo_pixel.cpp.obj"
+  "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/main.cpp" "C:/Users/deyla/OneDrive/Escritorio/materias tec/semestre 5/datos 2/proyector 2/Proyecto2-datos2/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML_INCLUDE_DIR"
+  "/SFML-2.5.1/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Codigo.dir/BMP/Bmp.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Circulo.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Cuadrado.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Facade_funciones.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Figuras.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Filtros.cpp.obj"
+  "CMakeFiles/Codigo.dir/Funcionalidades/Giros.cpp.obj"
+  "CMakeFiles/Codigo.dir/Interfaz/Componentes.cpp.obj"
+  "CMakeFiles/Codigo.dir/Interfaz/Datos_juego.cpp.obj"
+  "CMakeFiles/Codigo.dir/Interfaz/Ventana.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Lista_Doble.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Lista_pixeles.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Matriz.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo_matriz.cpp.obj"
+  "CMakeFiles/Codigo.dir/lista_doblemente_enlazada/Nodo_pixel.cpp.obj"
+  "CMakeFiles/Codigo.dir/main.cpp.obj"
+  "Codigo.exe"
+  "Codigo.exe.manifest"
+  "Codigo.pdb"
+  "libCodigo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Codigo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
